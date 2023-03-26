45ecb21d9234e60031322fd47f57433aafef5e37
Haohan Yang <yanghaohanmail@gmail.com>
2023-03-26 16:42:15 +0200

    remove unused flag

--- Chat.Client/Program.cs
+++ Chat.Client/Program.cs
@@ -1,20 +1,5 @@
-﻿using System.CommandLine;
-using Chat.Client;
+﻿using Chat.Client;
 
-var usernameOption = new Option<String>(
-    name: "-u",
-    description: "Username",
-    getDefaultValue: () => "defaultUser"
-);
-
-var rootCommand = new RootCommand("Simple Chat app");
-rootCommand.AddOption(usernameOption);
-
-rootCommand.SetHandler(async (username) =>
-{
-    var baseUrl = "http://localhost:5101";
-    var client = new ChatClient(baseUrl);
-    await client.Run();
-}, usernameOption);
-
-await rootCommand.InvokeAsync(args);
+var baseUrl = "http://localhost:5101";
+var client = new ChatClient(baseUrl);
+await client.Run();
